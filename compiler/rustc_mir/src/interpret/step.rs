b195714faecf684a0ad955849c80de06922b52ec
Yuanjian CHEN <chenyj@xcvmbyte.com>
2023-12-22 10:23:18 +0800

    android: set abort message

--- compiler/rustc_mir/src/interpret/step.rs
+++ compiler/rustc_mir/src/interpret/step.rs
@@ -38,9 +38,6 @@ impl<'mir, 'tcx: 'mir, M: Machine<'mir, 'tcx>> InterpCx<'mir, 'tcx, M> {
     /// Returns `true` as long as there are more things to do.
     ///
     /// This is used by [priroda](https://github.com/oli-obk/priroda)
-    ///
-    /// This is marked `#inline(always)` to work around adverserial codegen when `opt-level = 3`
-    #[inline(always)]
     pub fn step(&mut self) -> InterpResult<'tcx, bool> {
         if self.stack().is_empty() {
             return Ok(false);
